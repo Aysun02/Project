52233964a59d26415af71d13498fb45565338986
Aysun02 <aysunvh@code.edu.az>
2022-11-28 08:45:46 +0400

    added

---
+++ FlowerProjectP323/Controllers/BasketController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerProjectP323.Controllers
+{
+    public class BasketController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add()
+    }
+}
---
+++ FlowerProjectP323/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Policy;
+
+namespace FlowerProjectP323.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
